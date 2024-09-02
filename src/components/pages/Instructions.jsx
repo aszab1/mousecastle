744ec4544d251286f48b5883d0a35c2f718b2ab8
ataabatay <aliataabatay@gmail.com>
2024-09-02 17:04:56 +0100

    completed the instructions page with dummy content and placeholder design links to the questions overview page

--- src/components/pages/Instructions.jsx
+++ src/components/pages/Instructions.jsx
@@ -1,8 +1,25 @@
+import { Link } from 'react-router-dom';
+import InstructionsImg from '../../assets/images/mates-mouses.jpg'
+
 export const Instructions = () => {
   return (
     <>
-      <section id="instructions" className="">
-        <h1>Instructions Page</h1>
+      <section id="instructions" className="flex flex-col gap-8 h-full items-center justify-center">
+        <img src={InstructionsImg} alt="instructions-page-image"/>
+        <h1 className='text-2xl'>Here&apos;s how this works</h1>
+        <ul className='flex flex-col gap-4 w-5/6'>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+        </ul>
+        <Link 
+        to='/questions'
+        >
+          <button className='bg-blue-100 py-4 px-16 rounded-md'>Start</button>
+        </Link>
       </section>
     </>
   );
