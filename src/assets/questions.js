9b55c430651469f484963078f8fa05f1e71a4d61
ataabatay <aliataabatay@gmail.com>
2024-09-02 21:00:52 +0100

    added the instructions page, created the questions data file but needs work to be completed was waiting Adrienn for the answers to decide how to store them, added the fonts to the project

---
+++ src/assets/questions.js
@@ -0,0 +1,7 @@
+export const Questions = () => [
+  {
+    id: 0, 
+    question: "Baron Alexander Castlehouse has found some lovely wall paintings in the entrance hall. What is the part of the angel's foot that is closest to you?",
+    answer_length: '6'
+  }
+]
--- src/components/pages/Instructions.jsx
+++ src/components/pages/Instructions.jsx
@@ -1,24 +1,25 @@
 import { Link } from 'react-router-dom';
-import InstructionsImg from '../../assets/images/mates-mouses.jpg'
+import InstructionsImg from '../../assets/images/mates-mouses.jpg';
 
 export const Instructions = () => {
   return (
     <>
       <section id="instructions" className="flex flex-col gap-8 h-full items-center justify-center">
-        <img src={InstructionsImg} alt="instructions-page-image"/>
-        <h1 className='text-2xl'>Here&apos;s how this works</h1>
-        <ul className='flex flex-col gap-4 w-5/6'>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-        </ul>
-        <Link 
-        to='/questions'
-        >
-          <button className='bg-blue-100 py-4 px-16 rounded-md'>Start</button>
+        <img src={InstructionsImg} alt="instructions-page-image" />
+        <h1 className="text-4xl italic bold">Welcome!</h1>
+        <div className="welcome-text flex flex-col gap-4 text-center bold">
+          <p>My name is Prince Magnus</p>
+          <p>Mousecastle, the Governer of The Royal Palace.</p>
+          <p> Today you can explore some of the secrets of my palace if you find the 7 game boxes upstairs.</p>
+          <p>
+            If you solve the riddle and discover the password, you can collect your prize in the Tourinform Office!
+          </p>
+          <p> I can help you with a clue to the password:</p>
+          <p className='italic text-md text-amber-500'> It&apos;s my friend, Count Chester Gorgonzola&apos;s favorite dessert! </p>
+          <p>Good luck and have fun!</p>
+        </div>
+        <Link to="/questions">
+          <button className="bg-amber-500 py-4 px-16 rounded-md">Start</button>
         </Link>
       </section>
     </>
