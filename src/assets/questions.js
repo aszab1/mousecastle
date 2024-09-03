c73e5d3faae938a52ed802138e503a5b5b149c4c
ataabatay <aliataabatay@gmail.com>
2024-09-03 11:55:14 +0100

    added the questions object

--- src/assets/questions.js
+++ src/assets/questions.js
@@ -1,7 +1,75 @@
+import Image1 from './images/pic3.jpeg';
+import Image2 from './images/mates-mouses.jpg';
+import Image3 from './images/writing-mouse1.jpg';
+import Image4 from './images/mouse1-nobg.png';
+import Image5 from './images/welcome-mouse-pic.jpg';
+import Image6 from './images/pic4.jpeg';
+import Image7 from './images/mouse1-withbg.jpg';
+
 export const Questions = () => [
   {
-    id: 0, 
-    question: "Baron Alexander Castlehouse has found some lovely wall paintings in the entrance hall. What is the part of the angel's foot that is closest to you?",
-    answer_length: '6'
-  }
-]
+    id: 0,
+    img_url: Image1,
+    eng_question:
+      "Baron Alexander Castlehouse has found some lovely wall paintings in the entrance hall. What is the part of the angel's foot that is closest to you?",
+    eng_answer: 'BIG TOE',
+    hun_question: 'Itt az elötérben Báró Kastély-Hazy Sándor barátom fedezte fel ezeket a szép,',
+    hun_answer: 'LÁBUJJ',
+  },
+  {
+    id: 1,
+    img_url: Image2,
+    eng_question:
+      "When I was little, Minka grandma told me a story about how the Count built this big beautiful palace. At first, he built just a little bit. As time went on and the Count had more money he added on and on to the Palace. Now we can all fit really well! What is the Count's family name?",
+    eng_answer: 'GRASSALKOVICH',
+    hun_question: '',
+    hun_answer: 'ANTAL',
+  },
+  {
+    id: 2,
+    img_url: Image3,
+    eng_question:
+      'Here in the Palace, we have had many guests. King I. Franz Joseph spent lots of time here but he usually always worked. I found that very boring! Here on the picture what I am doing?',
+    eng_answer: 'YAWNING',
+    hun_question: '',
+    hun_answer: 'ÁSITOK',
+  },
+  {
+    id: 3,
+    img_url: Image4,
+    eng_question:
+      'If you would have looked out of this window 150 years ago then you would have seen a gorgeous lake. They named the lake after the animals that swam on it. What is the name of this animal?',
+    eng_answer: 'SWAN LAKE',
+    hun_question: '',
+    hun_answer: 'HATTYÚS TÓ',
+  },
+  {
+    id: 4,
+    img_url: Image5,
+    eng_question:
+      'Queen Elizabeth had beautiful long hair. It took hours to brush everyday. While her maids brushed her hair she would study languages, Greek and Ancient Greek. Here, in Gödöll Queen Elisabeth spoke this language with her youngest daughter, Marie Valerie:',
+    eng_answer: 'HUNGARIAN',
+    hun_question: '',
+    hun_answer: 'MAGYARUL',
+  },
+  {
+    id: 5,
+    img_url: Image6,
+    eng_question: 'Queen Elizabeth was very fit because she loved sports. Her favorite was...',
+    eng_answer: 'HORSE RIDING',
+    hun_question: '',
+    hun_answer: 'LOVAGLÁS',
+  },
+  {
+    id: 6,
+    img_url: Image7,
+    eng_question:
+      "The royal couple's youngest daughter, Marie Valerie, lived in this beautiful powder blue suite. Princess Marie Valerie had a very strict schedule, she had a lot to study. My friend Count Chester Gorgonzola was often inspired by the Princess. What will the Count do when he goes home?",
+    eng_answer: 'PAINT',
+    hun_question: '',
+    hun_answer: 'FESTENI',
+  },
+];
+
+export const englishPassword = 'BUTTER COOKIE';
+export const hungarianPassword = 'MÁKOS BEJGLI';
